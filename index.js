81fdd9aeb147574cd2eb5a0ca49f4305649660cd
Rene Ramirez <rdejramirez@gmail.com>
2022-10-04 17:04:46 -0500

    test

--- index.js
+++ index.js
@@ -4,23 +4,31 @@ const axios = require('axios')
 const app = express()
 
 const scrapeHat = (req, res) => {
-    let url = req.query.url
+  let url = req.query.url
   axios
     .get(url)
-    .then(response => {
-        let posibleLink = response.data;
-        let positionInit = posibleLink.search('https://www.pornhat.com/embed')
-        posibleLink = posibleLink.substring(positionInit)
-        let positionFinal =  posibleLink.search('"')
-        posibleLink = posibleLink.substring(0, positionFinal)
-        res.status(200).json({
-            video_url: posibleLink
-        })
+    .then((response) => {
+      let posibleLink = response.data
+      let positionInit = posibleLink.search('https://www.pornhat.com/embed')
+      posibleLink = posibleLink.substring(positionInit)
+      let positionFinal = posibleLink.search('"')
+      posibleLink = posibleLink.substring(0, positionFinal)
+      res.status(200).json({
+        video_url: posibleLink,
+      })
     })
     .catch((err) => {
       res.json(err)
     })
 }
+
+const render = (req, res) => {
+  res.json({
+    message: 'Welcome to my api',
+  })
+}
+
+app.get('/', render)
 
 app.get('/hat', scrapeHat)
 
