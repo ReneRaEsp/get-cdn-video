134989e737ff7555899c4e616b1aae90c7ffaefc
Rene Ramirez <rdejramirez@gmail.com>
2022-10-04 19:47:21 -0500

    test 2

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 const express = require('express')
 const axios = require('axios')
+const request = require('request')
 
 const app = express()
 
-const scrapeHat = (req, res) => {
+const scrapeHat = async (req, res) => {
   let url = req.query.url
-  axios
+  let posibleLink
+  await axios
     .get(url)
     .then((response) => {
-      let posibleLink = response.data
-      let positionInit = posibleLink.search('https://www.pornhat.com/embed')
+      posibleLink = response.data
+      let positionInit = posibleLink.search('download-link')
+      posibleLink = posibleLink.substring(positionInit)
+      positionInit = posibleLink.search('http')
       posibleLink = posibleLink.substring(positionInit)
       let positionFinal = posibleLink.search('"')
       posibleLink = posibleLink.substring(0, positionFinal)
-      res.status(200).json({
-        video_url: posibleLink,
-      })
     })
     .catch((err) => {
       res.json(err)
     })
+
+  request({ url: posibleLink, followRedirect: false }, (err, response, body) => {
+    if (!err) {
+      posibleLink = response.headers.location
+      res.status(200).json({
+        video_url: posibleLink,
+      })
+    } else {
+      res.status(404).json({
+        video_url: 'not found',
+      })
+    }
+  })
 }
 
 const render = (req, res) => {
